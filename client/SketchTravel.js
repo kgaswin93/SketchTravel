2a4845d7a415889ae29ab2b607b9f606a1d52bf0
kgaswin93 <kgaswin93@localhost.localdomain>
2016-02-07 07:51:39 +0530

    Adding change to obtain route

--- client/SketchTravel.js
+++ client/SketchTravel.js
@@ -1,72 +1,81 @@
 
 if (Meteor.isClient) {
+
+  var directionsDisplay;
+  var directionsService;
+  var map;
+
+  var MAP_ZOOM = 15;
+
+  Meteor.startup(function() {
+
+  });
+
   // This code only runs on the client
   Meteor.subscribe("tasks");
 
   Template.body.helpers({
-    tasks: function () {
-      if (Session.get("hideCompleted")) {
-        // If hide completed is checked, filter tasks
-        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-      } else {
-        // Otherwise, return all of the tasks
-        return Tasks.find({}, {sort: {createdAt: -1}});
-      }
-    },
-    hideCompleted: function () {
-      return Session.get("hideCompleted");
-    },
-    incompleteCount: function () {
-      return Tasks.find({checked: {$ne: true}}).count();
-    }
+
   });
 
   Template.body.events({
-    "submit .new-task": function (event) {
-      // Prevent default browser form submit
-      event.preventDefault();
 
-      // Get value from form element
-      var text = event.target.text.value;
-
-      // Insert a task into the collection
-      Meteor.call("addTask", text);
-
-      // Clear form
-      event.target.text.value = "";
-    },
-    "change .hide-completed input": function (event) {
-      Session.set("hideCompleted", event.target.checked);
-    }
-  });
-
-  Template.task.helpers({
-    isOwner: function () {
-      return this.owner === Meteor.userId();
-    }
-  });
-
-  Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Meteor.call("setChecked", this._id, ! this.checked);
-    },
-    "click .delete": function () {
-      Meteor.call("deleteTask", this._id);
-    },
-    "click .toggle-private": function () {
-      Meteor.call("setPrivate", this._id, ! this.private);
-    }
   });
 
   Template.travelForm.events({
-    "click .travelFormSubmit": function () {
+    "click .travelFormSubmit": function (event, template) {
       // Set the checked property to the opposite of its current value
-      alert("hello");
+      //alert(template.find(".fromPlace").value);
+      Template.map_canvas.getdirection(template.find(".fromPlace").value,template.find(".toPlace").value);
+      return false;
     }
   });
 
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
+
+
+  Template.map_canvas.onCreated(function() {
+    self = this;
+      directionsDisplay = new google.maps.DirectionsRenderer();
+      // Create and move the marker when latLng changes.
+
+        self.autorun(function() {
+        var latLng = Geolocation.latLng();
+        if (! latLng)
+          return;
+
+          var mapOptions = {
+            center: new google.maps.LatLng(latLng.lat, latLng.lng),
+            zoom: 10,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+          var map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
+          directionsDisplay.setMap(map);
+
+          var marker = new google.maps.Marker({
+                  position: latLng,
+                  setMap: map_canvas,
+                  //etVisible: true
+         });
+
+         map.setCenter(marker.getPosition());
+         map.setZoom(MAP_ZOOM);
+      });
+  });
+
+  Template.map_canvas.getdirection = function(start,end) {
+    directionsService = new google.maps.DirectionsService();
+        var request = {
+            origin:start,
+            destination:end,
+            travelMode: google.maps.DirectionsTravelMode.DRIVING
+        };
+        directionsService.route(request, function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+          }
+        });
+  }
 }
